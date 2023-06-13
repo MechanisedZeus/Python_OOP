d755535b3f24a25485e7e815f13792fd2d49157d
Zeus2259 <evandyball@hotmail.com>
2023-06-13 21:59:32 +0100

    removed some development funcions from Queue

--- Queue.py
+++ Queue.py
@@ -20,30 +20,16 @@ class Queue:
             self.front += 1
         return output
 
-    def show (self):
-        return str(self.data) + str(self.front) + str(self.end)
 
-
-
-    def check_length (self):
-        if len(self.data) < self.size:
-            return True
-        else:
-            return False
     
 
 def demo():
     q = Queue(2)
     q.enQueue(10)
-    print(q.show())
     q.enQueue(45)
-    print(q.show())
     print(q.deQueue())
-    print(q.show())
     print(q.deQueue())
     q.enQueue(88)
-    print(q.show())
     q.enQueue(98)
-    print(q.show())
 
 demo()    
